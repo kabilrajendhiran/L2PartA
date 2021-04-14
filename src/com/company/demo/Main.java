c09366fd86ccd0b29a434a5c2b6f8d80f606793d
kabildeveloper <kabildeveloper@gmail.com>
2021-04-14 11:29:51 +0530

    Question2 Union Array Finished

--- src/com/company/demo/Main.java
+++ src/com/company/demo/Main.java
@@ -3,8 +3,10 @@ package com.company.demo;
 public class Main {
 
     public static void main(String[] args) {
-        Question1.printPattern("PROGRAM");
-
+        /*Question1.printPattern("PROGRAM");*/
+        Question2 question2 = new Question2();
+        question2.unionArray();
+        question2.unionArrayWithTreeSet();
 
 
     }
--- src/com/company/demo/Question2.java
+++ src/com/company/demo/Question2.java
@@ -1,5 +1,59 @@
 package com.company.demo;
 
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Question2 {
+    int[] firstArray = {4,5,9,13,15,17};
+    int[] secondArray = {4,8,9,11,16,17};
+    int[] unionArray = new int[firstArray.length+secondArray.length];
+
+
+    //Given Question: Using Array
+    public void unionArray()
+    {
+        int count = 0;
+        for (int i = 0; i < firstArray.length; i++) {
+            if(firstArray[i]==secondArray[i])
+            {
+                unionArray[count++] = firstArray[i];
+            }
+            else if(firstArray[i]>secondArray[i])
+            {
+                unionArray[count++]= secondArray[i];
+                unionArray[count++] = firstArray[i];
+
+            }
+            else if(firstArray[i]<secondArray[i])
+            {
+                unionArray[count++]= firstArray[i];
+                unionArray[count++] = secondArray[i];
+
+            }
+        }
+
+        System.out.println(Arrays.toString(unionArray));
+    }
+
+
+    // Another Method using tree set
+    public void unionArrayWithTreeSet()
+    {
+        Set<Integer> set = new TreeSet();
+        for (int i = 0; i < firstArray.length; i++) {
+            set.add(firstArray[i]);
+            set.add(secondArray[i]);
+        }
+
+        for (int i: set) {
+            System.out.print(i+ " ");
+        }
+    }
+
+
 
 }
