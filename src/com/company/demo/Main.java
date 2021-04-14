7bb305c7da6bd28627993a96400eb0096e87fae6
kabildeveloper <kabildeveloper@gmail.com>
2021-04-14 08:14:31 +0530

    Moved to new Class

--- src/com/company/demo/Main.java
+++ src/com/company/demo/Main.java
@@ -3,34 +3,11 @@ package com.company.demo;
 public class Main {
 
     public static void main(String[] args) {
-	    String s = "PROGRAM";
-	    int length = s.length();
-	    int space_length = length/2;
+        Question1.printPattern("PROGRAM");
 
-        for (int i = 0; i < length/2; i++) {
-            printSpace(i);
-            System.out.print(s.charAt(i));
-            printSpace(2*(space_length-i)-1);
-            System.out.print(s.charAt(length-1-i));
-            System.out.println("");
-        }
-        printSpace(space_length);
-        System.out.println(s.charAt(space_length));
 
-        for (int i = 0; i < length/2; i++) {
-            printSpace(space_length-i-1);
-            System.out.print(s.charAt(space_length - i-1));
-            printSpace((i*2)+1);
-            System.out.print(s.charAt(space_length+i+1));
-            System.out.println("");
-        }
-        
+
     }
 
-    public static void printSpace(int n)
-    {
-        for (int i = 0; i < n; i++) {
-            System.out.print(" ");
-        }
-    }
+
 }
---
+++ src/com/company/demo/Question2.java
@@ -0,0 +1,5 @@
+package com.company.demo;
+
+public class Question2 {
+
+}
