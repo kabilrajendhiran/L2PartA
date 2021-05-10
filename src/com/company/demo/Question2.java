9f6519973603046e1e56748e9d9d4366c63bb044
kabildeveloper <kabildeveloper@gmail.com>
2021-05-10 18:42:05 +0530

    Question 2 is updated

--- src/com/company/demo/Question2.java
+++ src/com/company/demo/Question2.java
@@ -13,30 +13,40 @@ public class Question2 {
     //Given Question: Using Array
     public void unionArray(int[] firstArray, int[] secondArray)
     {
-        int count = 0;
-        int[] unionArray = new int[firstArray.length+secondArray.length];
-        for (int i = 0; i < firstArray.length; i++) {
-            if(firstArray[i]==secondArray[i])
-            {
-                unionArray[count++] = firstArray[i];
-            }
-            else if(firstArray[i]>secondArray[i])
-            {
-                unionArray[count++]= secondArray[i];
-                unionArray[count++] = firstArray[i];
+        int f = firstArray.length;
+        int l = secondArray.length;
+        int n = f+l;
+        int[] unionArray = new int[n];
+        int a=0,b=0;
 
-            }
-            else if(firstArray[i]<secondArray[i])
+        for (int i = 0; i < n; i++) {
+            if(a<f&&b<l)
             {
-                unionArray[count++]= firstArray[i];
-                unionArray[count++] = secondArray[i];
-
+                if(firstArray[a]>secondArray[b])
+                {
+                    unionArray[i] = secondArray[b];
+                    b++;
+                }
+                else if(firstArray[a]<secondArray[b])
+                {
+                    unionArray[i] = firstArray[a];
+                    a++;
+                }
+                else if(firstArray[a]==secondArray[b])
+                {
+                    unionArray[i] = firstArray[a];
+                    a++;
+                    b++;
+                }
+            }
+            else if(a==f && b==l)
+            {
+                break;
             }
         }
-
-
         System.out.println(Arrays.toString(unionArray));
     }
+
 
     // Another Method using tree set
     public void unionArrayWithTreeSet(int[] firstArray, int[] secondArray)
