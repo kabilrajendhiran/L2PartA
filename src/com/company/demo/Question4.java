5e98aa77b999800e8c77a62135a99a2e23f2ebcd
kabildeveloper <kabildeveloper@gmail.com>
2021-05-11 20:36:47 +0530

    Question 4 is refactored

--- src/com/company/demo/Question4.java
+++ src/com/company/demo/Question4.java
@@ -1,68 +1,53 @@
 package com.company.demo;
 
 public class Question4 {
-    public static void frequencyOfNumbersInArray(int[] arr)
-    {
+    public static void frequencyOfNumbersInArray(int[] arr) {
         int value = arr[0];
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            int x = i+1;
-            while (x< arr.length && arr[i]!=arr[x])
-            {
+            int x = i + 1;
+            while (x < arr.length && arr[i] != arr[x]) {
                 x++;
             }
-            if(x< arr.length)
-            {
-                if(value==arr[x])
-                {
+            if (x < arr.length) {
+                if (value == arr[x]) {
                     count++;
-                }
-                else if(value!=arr[x])
-                {
+                } else if (value != arr[x]) {
                     /*System.out.println(value+" : "+count);*/
-                    value =arr[i];
-                    count=1;
+                    value = arr[i];
+                    count = 1;
                 }
-                swap(i+1,x,arr);
+                swap(i + 1, x, arr);
                 /*System.out.println(i+" "+" "+x);*/
-            }
-            else if(i!= arr.length-1)
-            {
-                if(value==arr[i])
-                {
+            } else if (i != arr.length - 1) {
+                if (value == arr[i]) {
                     count++;
+                } else {
+                    count = 1;
                 }
-                else {
-                    count=1;
-                }
-                System.out.println(arr[i]+" : "+count);
-            }
-            else {
-                System.out.println(arr[i]+" : "+count);
+                System.out.println(arr[i] + " : " + count);
+            } else {
+                System.out.println(arr[i] + " : " + count);
             }
 
         }
     }
 
-    private static void sortArr(int[] a)
-    {
+    private static void sortArr(int[] a) {
         for (int i = 1; i < a.length; i++) {
-            int j=i-1;
+            int j = i - 1;
             int k = a[i];
 
-            while (j>=0 && a[j]>k )
-            {
-                a[j+1] = a[j];
-                j = j-1;
+            while (j >= 0 && a[j] > k) {
+                a[j + 1] = a[j];
+                j = j - 1;
             }
-            a[j+1] = k;
+            a[j + 1] = k;
         }
 
     }
 
-
-    public static void swap(int i,int j,int[] arr)
-    {
+    public static void swap(int i, int j, int[] arr) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
