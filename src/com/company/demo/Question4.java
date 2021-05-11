e9d361d27a58cc2487af4ae6ef5d20e5aa0e98cf
kabildeveloper <kabildeveloper@gmail.com>
2021-05-11 20:31:18 +0530

    Question 4 is updated

--- src/com/company/demo/Question4.java
+++ src/com/company/demo/Question4.java
@@ -1,38 +1,6 @@
 package com.company.demo;
 
 public class Question4 {
-  /*  public static void frequencyOfNumbersInArray(int[] arr)
-    {
-        sortArr(arr);
-        for (int i = 0; i < arr.length-1; i++) {
-            int count = 1;
-            while (i< arr.length-1 && arr[i]==arr[i+1])
-            {
-                i++;
-                count++;
-            }
-
-            System.out.println(arr[i]+"->"+count);
-        }
-    }
-
-    private static void sortArr(int[] a)
-    {
-        for (int i = 1; i < a.length; i++) {
-            int j=i-1;
-            int k = a[i];
-
-            while (j>=0 && a[j]>k )
-            {
-                a[j+1] = a[j];
-                j = j-1;
-            }
-            a[j+1] = k;
-        }
-
-    }
-*/
-
     public static void frequencyOfNumbersInArray(int[] arr)
     {
         int value = arr[0];
@@ -75,6 +43,23 @@ public class Question4 {
 
         }
     }
+
+    private static void sortArr(int[] a)
+    {
+        for (int i = 1; i < a.length; i++) {
+            int j=i-1;
+            int k = a[i];
+
+            while (j>=0 && a[j]>k )
+            {
+                a[j+1] = a[j];
+                j = j-1;
+            }
+            a[j+1] = k;
+        }
+
+    }
+
 
     public static void swap(int i,int j,int[] arr)
     {
